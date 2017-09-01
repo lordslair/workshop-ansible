548d7d9a2f5cfa9cac87145132f4c546b9733a5c
lordslair <rvandepo+github@gmail.com>
2017-09-01 17:14:07 +0200

    Initial push of check_hosts

---
+++ check_hosts.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Term::ANSIColor;
+use Data::Dumper;
+
+my $servers = `source ~/OpenStack/openrc.sh && nova list 2>&1 | grep -Ev "server00|tool" | awk '{ print \$4 }' | grep server | tr '\n' ' '`;
+my @servers = split / /, $servers;
+
+foreach my $server (@servers)
+{   
+    my $rawload = `/root/DBaaS-TS/get-load-dbaas-ts.sh $server | awk '{ print \$2 }'`;
+
+    if ( $rawload !~ /^\s*$/ )
+    {
+        my $load = sprintf("%.2f",$rawload);
+        chomp ($load);
+        my $running = `source ~/OpenStack/openrc.sh && nova list 2>&1 | grep -c $server-fresh`;
+        if ( $load >= 0.75 )
+        {   
+            if ( $running == 0 )
+            {   
+                if ( $server =~ /fresh/ ) 
+                {
+                    printf("Host %-25s on fire (%s)  [but already 2nd gen, not spawn-looping]\n", colored($server, 'bold cyan'), colored($load, 'bold red'));
+                }
+                else
+                {
+                    printf("Host %-25s on fire (%s)  [spawning a fresh new instance]\n", colored($server, 'bold cyan'), colored($load, 'bold red'));
+                    `source ~/OpenStack/openrc.sh && nova boot --flavor vps-ssd-1 --image 'Debian 8' --user-data ~/OpenStack/post-install.yaml $server-fresh 2>&1`;
+                }
+            }
+            else
+            {   
+                printf("Host %-25s on fire (%s)  [but a clone was already spawned]\n", colored($server, 'bold cyan'), colored($load, 'bold red'));
+            }
+        }
+        else 
+        {   
+            if ( $running == 0 )
+            {   
+                printf("Host %-25s is fine (%s)\n", colored($server, 'bold cyan'), colored($load, 'bold green'));
+            }
+            else
+            {   
+                printf("Host %-25s is fine (%s)  [so %s will be destroyed]\n", colored($server, 'bold cyan'), colored($load, 'bold green'), "$server-fresh");
+                `source ~/OpenStack/openrc.sh && nova delete '$server-fresh' 2>&1`;
+            }
+        }
+    } else { printf("Host %-25s not in DBaaS-TS [be patient, or do something!]\n", colored($server, 'bold cyan')) }
+}
