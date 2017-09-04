29df78faaea0ad104e38d461871fe52930ee87ed
lordslair <lordslair@users.noreply.github.com>
2017-09-04 18:25:34 +0200

    Changed instance naming

--- DBaaS-TS/get-all-load-dbaas-ts.sh
+++ DBaaS-TS/get-all-load-dbaas-ts.sh
@@ -5,5 +5,7 @@ curl -s -XPOST https://opentsdb-gra1.tsaas.ovh.com/api/query/last \
      -H "Content-Type: application/json" \
      -d '{ 
             "queries": [{"metric":"loadavg"}] 
-         }'| python -mjson.tool  | perl -lne 'print $1 if /(server\d\d)/; print $1 if /(\d[.]\d*)/' | tr '\n' ' '
-echo
+         }'| \
+python -mjson.tool | \
+perl -lne 'print $1 if /(server[-]?\d\d)/; print $1 if /(\d[.]\d*)/' | \
+tr '\n' ' '
