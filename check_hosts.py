102481997a6f771c41d346b9478efb8a87d536ec
lordslair <rvandepo+github@gmail.com>
2017-09-26 15:13:30 +0000

    Added Python version of script

---
+++ check_hosts.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import sys
+import re
+import os
+import commands
+import base64
+
+sys.path.insert(0, '/root/workshop-ansible/OpenStack/')
+from credentials import get_nova_credentials_v2
+
+from termcolor import colored # python-termcolor
+from novaclient.client import Client
+
+credentials = get_nova_credentials_v2()
+nova_client = Client(**credentials)
+servers     = nova_client.servers.list()
+image       = nova_client.images.find(name="Debian 8")
+flavor      = nova_client.flavors.find(name="vps-ssd-1")
+data        = open("/root/workshop-ansible/OpenStack/post-install.yaml","r")
+
+for server in servers:
+#    match = re.search(r"(\w*?-\d[1-9])", server.name)
+    match = re.search(r"(\w*?-\d*)", server.name)
+    if match:
+        rawload     = commands.getstatusoutput("/root/workshop-ansible/DBaaS-TS/get-load-dbaas-ts.sh " + server.name + " | awk '{ print $2 }'")
+        server_cyan = colored(server.name, 'cyan' , attrs=['bold'])
+
+        if rawload[1]:
+            load        = float(rawload[1])
+            load_red    = colored('{0:.2f}'.format(load), 'red'  , attrs=['bold'])
+            load_green  = colored('{0:.2f}'.format(load), 'green', attrs=['bold'])
+            fresh       = server.name + '-fresh'
+            grep        = any(server.name == fresh for server in servers) 
+
+            if load >= 0.75:
+                if grep == False:
+                    if re.search(r"fresh", server.name):
+                        print("Host %-36s on fire (%s)  [but already 2nd gen, not spawn-looping]"% (server_cyan, load_red))
+                    else:
+                        print("Host %-36s on fire (%s)  [spawning a fresh new instance]"% (server_cyan, load_red))
+                        server = nova_client.servers.create(name = server.name + '-fresh', 
+                                                            image = image.id, 
+                                                            flavor = flavor.id,
+                                                            userdata = data)
+                else:
+                    print("Host %-36s on fire (%s)  [but a clone was already spawned]"% (server_cyan, load_red))
+            else:
+                if grep == False:
+                    print("Host %-36s is fine (%s)"% (server_cyan, load_green))
+                else:
+                    print("Host %-36s is fine (%s)  [so %s will be destroyed]"% (server_cyan, load_green, server.name + '-fresh'))
+
+        else:
+            print("Host %-36s not in DBaaS-TS [be patient, or do something!]"% (server_cyan))
