e427a989a8667cf123b8b4419d0dafa70da9211d
lordslair <rvandepo+github@gmail.com>
2017-10-02 14:23:24 +0000

    Added SMS function to notify on spawn/delete

--- check_hosts.py
+++ check_hosts.py
@@ -9,8 +9,12 @@ import base64
 sys.path.insert(0, '/root/workshop-ansible/OpenStack/')
 from credentials import get_nova_credentials_v2
 
+sys.path.insert(0, '/root/workshop-ansible/SMS/')
+from sms import send_sms
+
 from termcolor import colored # python-termcolor
 from novaclient.client import Client
+from pprint import pprint
 
 credentials = get_nova_credentials_v2()
 nova_client = Client(**credentials)
@@ -20,7 +24,7 @@ flavor      = nova_client.flavors.find(name="vps-ssd-1")
 data        = open("/root/workshop-ansible/OpenStack/post-install.yaml","r")
 
 for server in servers:
-    match = re.search(r"(\w*?-\d[1-9])", server.name)
+    match = re.search(r"(\w*?-\d[0-9])", server.name)
     if match:
         rawload     = commands.getstatusoutput("/root/workshop-ansible/DBaaS-TS/get-load-dbaas-ts.sh " + server.name + " | awk '{ print $2 }'")
         server_cyan = colored(server.name, 'cyan' , attrs=['bold'])
@@ -42,6 +46,7 @@ for server in servers:
                                                             image = image.id, 
                                                             flavor = flavor.id,
                                                             userdata = data)
+                        sms = send_sms('Workload too high. Spawning new instance ' + fresh)
                 else:
                     print("Host %-36s on fire (%s)  [but a clone was already spawned]"% (server_cyan, load_red))
             else:
@@ -51,6 +56,7 @@ for server in servers:
                     print("Host %-36s is fine (%s)  [so %s will be destroyed]"% (server_cyan, load_green, server.name + '-fresh'))
                     server = nova_client.servers.find(name=fresh)
                     nova_client.servers.delete(server.id)
+                    sms = send_sms('Workload back to normal. Deleting ' + fresh)
 
         else:
             print("Host %-36s not in DBaaS-TS [be patient, or do something!]"% (server_cyan))
